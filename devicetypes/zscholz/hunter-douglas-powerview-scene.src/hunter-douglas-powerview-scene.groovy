58c3dbda4a984c6fdc7156e8c87c270706fdf3a0
zscholz <36722570+zscholz@users.noreply.github.com>
2021-11-07 21:17:45 -0700

    Update hunter-douglas-powerview-scene.groovy

--- devicetypes/zscholz/hunter-douglas-powerview-scene.src/hunter-douglas-powerview-scene.groovy
+++ devicetypes/zscholz/hunter-douglas-powerview-scene.src/hunter-douglas-powerview-scene.groovy
@@ -1,7 +1,7 @@
 /**
  * Hunter Douglas PowerView Scene (device handler)
  * Copyright (c) 2017 Johnvey Hwang
- * Updated 2021 Zach Scholz
+ * Modified 2021 Zach Scholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
